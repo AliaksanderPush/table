b9498849f34b2e3aff4b5fd4e585f9f7ebaa3e46
Sash <tribeca1983@gmail.com>
2021-08-29 22:24:33 +0300

    add yup

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -9,7 +9,7 @@ import * as yup from "yup";
 
 
 export default class Auth extends React.Component {
- 
+ /*
 state = {
   imputs :[
   {name: "login", type: "text", text: "Введите логин:"},
@@ -18,13 +18,13 @@ state = {
   {name: "password", type: "password", text: "Повторите пароль:"}
   ]
 }
-
+*/
 
 
  alertInfo(login) {
   alert(`Ваши данные: ${login}`)
 }
-
+/*
  renderImputsHandler = () => {
   return this.state.imputs.map((input, index) => {
     let names = input["name"];
@@ -49,11 +49,14 @@ state = {
      );
    })
  }
-
+*/
 
   render() {
  const validationSchema = yup.object().shape({
   login: yup.string().typeError('Это должна быть строка').required('Обязательно для заполнения'),
+  email: yup.string().email('Введите верный email').required('Обязательное для заполнения'),
+  password:yup.string().typeError('Это должна быть строка').required('Обязательное для заполнения'),
+  confirmPassword:yup.string().oneOf([yup.ref('password')], 'Пароль не совпадает').required('Обязательное для заполнения')
 })
  
  //const {handleChange, handleBlur, values, handleSubmit, errors,
@@ -62,6 +65,9 @@ state = {
     <Formik
       initialValues={{
         login: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
 
       }}
       validationSchema={validationSchema}
@@ -78,7 +84,67 @@ state = {
         touched,
       }) => (
         <form onSubmit={handleSubmit}>
-           {this.renderImputsHandler()}
+            <label htmlFor="login">
+            <span>Введите логин:</span>
+            <input
+              name="login"
+              type="text"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.login}
+            />
+          </label>
+           {errors.login && touched.login && (
+            <span style={{ color: "red" }}> {errors.login} </span>
+          )}
+          <br/>
+          
+           <label htmlFor="email">
+            <span>Введите Email:</span>
+            <input
+              name="email"
+              type="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+          </label>
+           {errors.email && touched.email && (
+            <span style={{ color: "red" }}> {errors.email} </span>
+          )}
+          <br/>
+
+           <label htmlFor="password">
+            <span>Введите пароль:</span>
+            <input
+              name="password"
+              type="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+          </label>
+           {errors.password && touched.password && (
+            <span style={{ color: "red" }}> {errors.password} </span>
+          )}
+          <br/>
+
+          <label htmlFor="confirmPassword">
+            <span>Повторите пароль:</span>
+            <input
+              name="confirmPassword"
+              type="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.confirmPassword}
+            />
+          </label>
+           {errors.confirmPassword && touched.confirmPassword && (
+            <span style={{ color: "red" }}> {errors.confirmPassword} </span>
+          )}
+          <br/>
+
+
           <button type="submit">Зарегистрироваться</button>
         </form>
       )}
